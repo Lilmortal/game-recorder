e106d0498b7237a833050d2a81b11527b08571ef
Jack Tan <Jack.Tan@airnz.co.nz>
2019-09-04 17:12:16 +1200

    Added health check and enabling CORS

--- main.go
+++ main.go
@@ -18,11 +18,11 @@ func substr(input string, start int, length int) string {
 		return ""
 	}
 
-	if start + length > len(asRunes) {
+	if start+length > len(asRunes) {
 		length = len(asRunes) - start
 	}
 
-	return string(asRunes[start : start + length])
+	return string(asRunes[start : start+length])
 }
 
 func getAccountID(steamID string) (int, error) {
@@ -36,7 +36,9 @@ func getAccountID(steamID string) (int, error) {
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
 	opID := steam_go.NewOpenId(r)
+	log.Println("Mode", opID.Mode())
 	switch opID.Mode() {
 	case "":
 		http.Redirect(w, r, opID.AuthUrl(), 301)
@@ -45,44 +47,71 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	default:
 		steamID, err := opID.ValidateAndGetId()
 		if err != nil {
-			log.Printf("main.go: failed to get steam ID. \n %s", err.Error())
+			log.Fatalf("main.go: failed to get steam ID. %s", err.Error())
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
 		accountID, err := getAccountID(steamID)
 		if err != nil {
-			log.Printf("main.go: failed to get account ID from steam ID. \n %s", err.Error())
+			log.Fatalf("main.go: failed to get account ID from steam ID. %s", err.Error())
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
 		fmt.Printf("Account ID: %d", accountID)
-		// There is no way to query the match with the exact startTime, hence we save the queries immediately.
-		matches, err := http.Get(fmt.Sprintf("https://api.opendota.com/api/players/%d/matches?date=7", accountID))
-		if err != nil {
-			log.Printf("main.go: attempt to get recent matches for player with account ID %d failed. \n %s", accountID, err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-		responseBody, err := ioutil.ReadAll(matches.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			log.Printf("main.go: attempt to retrieve the response body from recent matches failed. \n %s", err.Error())
-		}
-
-		if string(responseBody) == "[]" {
-			emptyResponseErr := fmt.Sprintf("account ID %d seems to be incorrect as an empty response came back "+
-				"attempting to get recent matches.", accountID)
-			http.Error(w, emptyResponseErr, http.StatusInternalServerError)
-			log.Printf("main.go: " + emptyResponseErr)
-		} else {
-			w.Write([]byte(responseBody))
-			// To get date, Date(responseBody[0].start_time)
-		}
+		fmt.Fprintln(w, accountID)
 	}
+}
+
+func matchesHandler(w http.ResponseWriter, r *http.Request) {
+	// get accountID from request
+	accountID := 2
+
+	// There is no way to query the match with the exact startTime, hence we save the queries immediately.
+	matches, err := http.Get(fmt.Sprintf("https://api.opendota.com/api/players/%d/matches?date=7", accountID))
+	if err != nil {
+		log.Fatalf("main.go: attempt to get recent matches for player with account ID %d failed. %s", accountID, err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	responseBody, err := ioutil.ReadAll(matches.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalf("main.go: attempt to retrieve the response body from recent matches failed. %s", err.Error())
+	}
+
+	if string(responseBody) == "[]" {
+		emptyResponseErr := fmt.Sprintf("account ID %d seems to be incorrect as an empty response came back "+
+			"attempting to get recent matches.", accountID)
+		http.Error(w, emptyResponseErr, http.StatusInternalServerError)
+		log.Fatalf("main.go: " + emptyResponseErr)
+	} else {
+		w.Write([]byte(responseBody))
+		// To get date, Date(responseBody[0].start_time)
+	}
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("lala")
+	enableCors(&w)
+	fmt.Fprintln(w, `{
+		"userId": 1,
+		"id": 1,
+		"title": "delectus aut autem",
+		"completed": false
+	}`)
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
 }
 
 func main() {
 	fmt.Println("Running...")
 	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/healthCheck", healthCheckHandler)
+	http.HandleFunc("/matches", matchesHandler)
 	http.ListenAndServe(":8080", nil)
 }
---
+++ vendor/github.com/solovev/steam_go/auth.go
@@ -0,0 +1,139 @@
+package steam_go
+
+import (
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+)
+
+var (
+	steam_login = "https://steamcommunity.com/openid/login"
+
+	openId_mode       = "checkid_setup"
+	openId_ns         = "http://specs.openid.net/auth/2.0"
+	openId_identifier = "http://specs.openid.net/auth/2.0/identifier_select"
+
+	validation_regexp        = regexp.MustCompile("^(http|https)://steamcommunity.com/openid/id/[0-9]{15,25}$")
+	digits_extraction_regexp = regexp.MustCompile("\\D+")
+)
+
+type OpenId struct {
+	root      string
+	returnUrl string
+	data      url.Values
+}
+
+func NewOpenId(r *http.Request) *OpenId {
+	id := new(OpenId)
+
+	log.Println(id)
+	proto := "http://"
+	if r.TLS != nil {
+		proto = "https://"
+	}
+	id.root = proto + r.Host
+
+	uri := r.RequestURI
+	if i := strings.Index(uri, "openid"); i != -1 {
+		uri = uri[0 : i-1]
+	}
+	id.returnUrl = id.root + uri
+
+	switch r.Method {
+	case "POST":
+		id.data = r.Form
+	case "GET":
+		id.data = r.URL.Query()
+	}
+
+	// log.Println(id.data)
+	return id
+}
+
+func (id OpenId) AuthUrl() string {
+	data := map[string]string{
+		"openid.claimed_id": openId_identifier,
+		"openid.identity":   openId_identifier,
+		"openid.mode":       openId_mode,
+		"openid.ns":         openId_ns,
+		"openid.realm":      id.root,
+		"openid.return_to":  id.returnUrl,
+	}
+
+	log.Println("WEEEEEE")
+	i := 0
+	url := steam_login + "?"
+	for key, value := range data {
+		url += key + "=" + value
+		if i != len(data)-1 {
+			url += "&"
+		}
+		i++
+	}
+	log.Println("Steam url", url)
+	return url
+}
+
+func (id *OpenId) ValidateAndGetId() (string, error) {
+	if id.Mode() != "id_res" {
+		return "", errors.New("Mode must equal to \"id_res\".")
+	}
+
+	log.Println("DAta:", id.data)
+	if id.data.Get("openid.return_to") != id.returnUrl {
+		return "", errors.New("The \"return_to url\" must match the url of current request.")
+	}
+
+	params := make(url.Values)
+	params.Set("openid.assoc_handle", id.data.Get("openid.assoc_handle"))
+	params.Set("openid.signed", id.data.Get("openid.signed"))
+	params.Set("openid.sig", id.data.Get("openid.sig"))
+	params.Set("openid.ns", id.data.Get("openid.ns"))
+
+	split := strings.Split(id.data.Get("openid.signed"), ",")
+	for _, item := range split {
+		params.Set("openid."+item, id.data.Get("openid."+item))
+	}
+	params.Set("openid.mode", "check_authentication")
+
+	resp, err := http.PostForm(steam_login, params)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	response := strings.Split(string(content), "\n")
+	if response[0] != "ns:"+openId_ns {
+		return "", errors.New("Wrong ns in the response.")
+	}
+	if strings.HasSuffix(response[1], "false") {
+		return "", errors.New("Unable validate openId.")
+	}
+
+	openIdUrl := id.data.Get("openid.claimed_id")
+	if !validation_regexp.MatchString(openIdUrl) {
+		return "", errors.New("Invalid steam id pattern.")
+	}
+
+	return digits_extraction_regexp.ReplaceAllString(openIdUrl, ""), nil
+}
+
+func (id OpenId) ValidateAndGetUser(apiKey string) (*PlayerSummaries, error) {
+	steamId, err := id.ValidateAndGetId()
+	if err != nil {
+		return nil, err
+	}
+	return GetPlayerSummaries(steamId, apiKey)
+}
+
+func (id OpenId) Mode() string {
+	return id.data.Get("openid.mode")
+}
